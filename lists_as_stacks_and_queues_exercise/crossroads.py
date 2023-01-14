9e24f1417d34df262db5a9f439604159c5cfd266
DanieII <danielbogdanov1748@gmail.com>
2023-01-15 00:58:35 +0200

    Add solutions

--- lists_as_stacks_and_queues_exercise/crossroads.py
+++ lists_as_stacks_and_queues_exercise/crossroads.py
@@ -1,41 +1,68 @@
 from collections import deque
 
-
-def pass_func():
-    global cars_passed, current_green_light_time, over
-    cars_passed += 1
-    current_green_light_time -= characters
-    if current_green_light_time <= 0:
-        over = True
-
-
-green_light_time = int(input())
-free_window_time = int(input())
-cars = deque([])
-cars_passed = 0
-command = input()
+cars = deque()
+green_light = int(input())
+free_window = int(input())
+total_cars = 0
 
 while True:
+    command = input()
     if command == "END":
         break
 
     if command != "green":
         cars.append(command)
-    else:
-        over = False
-        current_green_light_time = green_light_time
-        for _ in range(len(cars)):
-            car = cars.popleft()
-            characters = len(car)
-            if characters > current_green_light_time + free_window_time:
-                last_character = car[current_green_light_time + free_window_time]
-                print("A crash happened!")
-                print(f"{car} was hit at {last_character}.")
-                quit()
+        continue
 
-            pass_func()
-            if over:
-                break
-    command = input()
-print("Everyone is safe.")
-print(f"{cars_passed} total cars passed the crossroads.")
+    current_green = green_light
+    while current_green > 0 and cars:
+        current_car = cars.popleft()
+        available_time = current_green + free_window
+        if len(current_car) > available_time:
+            last_character = current_car[available_time]
+            print(f"A crash happened!\n{current_car} was hit at {last_character}.")
+            quit()
+        current_green -= len(current_car)
+        total_cars += 1
+
+print(f"Everyone is safe.\n{total_cars} total cars passed the crossroads.")
+
+# Old solution
+# def pass_func():
+#     global cars_passed, current_green_light_time, over
+#     cars_passed += 1
+#     current_green_light_time -= characters
+#     if current_green_light_time <= 0:
+#         over = True
+#
+#
+# green_light_time = int(input())
+# free_window_time = int(input())
+# cars = deque([])
+# cars_passed = 0
+# command = input()
+#
+# while True:
+#     if command == "END":
+#         break
+#
+#     if command != "green":
+#         cars.append(command)
+#     else:
+#         over = False
+#         current_green_light_time = green_light_time
+#         for _ in range(len(cars)):
+#             car = cars.popleft()
+#             characters = len(car)
+#             if characters > current_green_light_time + free_window_time:
+#                 last_character = car[current_green_light_time + free_window_time]
+#                 print("A crash happened!")
+#                 print(f"{car} was hit at {last_character}.")
+#                 quit()
+#
+#             pass_func()
+#             if over:
+#                 break
+#     command = input()
+# print("Everyone is safe.")
+# print(f"{cars_passed} total cars passed the crossroads.")
--- lists_as_stacks_and_queues_exercise/cups_and_bottles.py
+++ lists_as_stacks_and_queues_exercise/cups_and_bottles.py
@@ -1,19 +1,39 @@
 from collections import deque
 
-cups = deque([int(x) for x in input().split()])
-bottles = deque([int(x) for x in input().split()])
+cups = deque(int(x) for x in input().split())
+bottles = [int(x) for x in input().split()]
 wasted = 0
 
 while cups and bottles:
-    current_cup = cups[0]
+    current_cup = cups.popleft()
     while current_cup > 0:
         current_bottle = bottles.pop()
         if current_bottle >= current_cup:
             wasted += current_bottle - current_cup
+            current_cup -= current_bottle
+            continue
         current_cup -= current_bottle
-    cups.popleft()
 if not cups:
-    print(f'Bottles: {" ".join([str(x) for x in bottles])}')
+    print(f"Bottles:", *bottles)
 else:
-    print(f'Cups: {" ".join([str(x) for x in cups])}')
+    print(f"Cups:", *cups)
 print(f'Wasted litters of water: {wasted}')
+
+# Old solution
+# cups = deque([int(x) for x in input().split()])
+# bottles = deque([int(x) for x in input().split()])
+# wasted = 0
+#
+# while cups and bottles:
+#     current_cup = cups[0]
+#     while current_cup > 0:
+#         current_bottle = bottles.pop()
+#         if current_bottle >= current_cup:
+#             wasted += current_bottle - current_cup
+#         current_cup -= current_bottle
+#     cups.popleft()
+# if not cups:
+#     print(f'Bottles: {" ".join([str(x) for x in bottles])}')
+# else:
+#     print(f'Cups: {" ".join([str(x) for x in cups])}')
+# print(f'Wasted litters of water: {wasted}')
