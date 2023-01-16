de0c41d5609f55314e502554a7def6c6c3f87819
DanieII <danielbogdanov1748@gmail.com>
2023-01-16 21:08:09 +0200

    Add solutions

--- tuples_and_sets_exercise/longest_intersection.py
+++ tuples_and_sets_exercise/longest_intersection.py
@@ -1,20 +1,35 @@
 lines = int(input())
-longest = None
-largest_length = 0
+longest = set()
 
 for _ in range(lines):
-    first, second = input().split("-")
+    first_values, second_values = [[int(el) for el in i.split(",")] for i in input().split("-")]
 
-    first_start, first_end = [int(x) for x in first.split(",")]
-    second_start, second_end = [int(x) for x in second.split(",")]
-    first_numbers = set(x for x in range(first_start, first_end + 1))
-    second_numbers = set(x for x in range(second_start, second_end + 1))
-    intersection = first_numbers.intersection(second_numbers)
-    if not longest:
-        longest = f"Longest intersection is {list(intersection)} with length {len(intersection)}"
-        largest_length = len(intersection)
-        continue
-    if len(intersection) > largest_length:
-        longest = f"Longest intersection is {list(intersection)} with length {len(intersection)}"
-        largest_length = len(intersection)
-print(longest)
+    first_range = set(range(first_values[0], first_values[1] + 1))
+    second_range = set(range(second_values[0], second_values[1] + 1))
+
+    intersection = first_range.intersection(second_range)
+    if len(intersection) > len(longest):
+        longest = intersection
+print(f"Longest intersection is {list(longest)} with length {len(longest)}")
+
+# Old solution
+# lines = int(input())
+# longest = None
+# largest_length = 0
+#
+# for _ in range(lines):
+#     first, second = input().split("-")
+#
+#     first_start, first_end = [int(x) for x in first.split(",")]
+#     second_start, second_end = [int(x) for x in second.split(",")]
+#     first_numbers = set(x for x in range(first_start, first_end + 1))
+#     second_numbers = set(x for x in range(second_start, second_end + 1))
+#     intersection = first_numbers.intersection(second_numbers)
+#     if not longest:
+#         longest = f"Longest intersection is {list(intersection)} with length {len(intersection)}"
+#         largest_length = len(intersection)
+#         continue
+#     if len(intersection) > largest_length:
+#         longest = f"Longest intersection is {list(intersection)} with length {len(intersection)}"
+#         largest_length = len(intersection)
+# print(longest)
